07a6cee79219bbe8356893f1bcb583a4f0a1faf8
Viruzz <nahuelsinka@gmail.com>
2024-02-18 16:15:45 -0300

    style button delete

--- src/components/view-image.tsx
+++ src/components/view-image.tsx
@@ -13,7 +13,7 @@ import 'lightgallery/css/lg-thumbnail.css'
 import 'lightgallery/css/lg-zoom.css'
 import { useCallback, useEffect, useRef } from 'react'
 import useUser from '@/hook/useUser'
-import { useRouter } from 'next/navigation'
+import { usePathname, useRouter } from 'next/navigation'
 
 interface Props {
   list: Array<{
@@ -30,6 +30,7 @@ interface Props {
 function ViewImage ({ list }: Props) {
   const { deleteFile } = useUser()
   const router = useRouter()
+  const pathname = usePathname()
   const lightGallery = useRef<any>(null)
 
   const onInit = useCallback((detail: any) => {
@@ -40,36 +41,32 @@ function ViewImage ({ list }: Props) {
 
   useEffect(() => {
     const nextBtn =
-      '<button type="button" aria-label="Next slide" class="lg-delete lg-icon"> todo </button>'
+      '<button type="button" aria-label="Next slide" class="lg-delete lg-icon flex justify-center items-center"> <svg width="21" height="21" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="m18 6-.8 12.013c-.071 1.052-.106 1.578-.333 1.977a2 2 0 0 1-.866.81c-.413.2-.94.2-1.995.2H9.994c-1.055 0-1.582 0-1.995-.2a2 2 0 0 1-.866-.81c-.227-.399-.262-.925-.332-1.977L6 6M4 6h16m-4 0-.27-.812c-.263-.787-.394-1.18-.637-1.471a2 2 0 0 0-.803-.578C13.938 3 13.524 3 12.694 3h-1.388c-.829 0-1.244 0-1.596.139a2 2 0 0 0-.803.578c-.243.29-.374.684-.636 1.471L8 6m6 4v7m-4-7v7" stroke="#999" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg> </button>'
 
     const $lgContainer = document.querySelector('.lg-toolbar')
     $lgContainer?.insertAdjacentHTML('beforeend', nextBtn)
-  }, [list])
 
-  const onSlideItemLoad = (detail) => {
-    // console.log('🚀 ~ onAfterOpen ~ detail:', detail)
-    const indexItem = detail.index
-    const ele = list[indexItem]
+    const fileType = pathname === '/app/imagen' ? 'image' : 'video'
 
     document.querySelector('.lg-delete')?.addEventListener('click', () => {
       const elementName = document.querySelector('.lg-sub-html')?.textContent
       if (elementName !== null && elementName !== undefined) {
-        console.log('delete')
-        deleteFile(ele.name, 'video')
+        deleteFile(elementName, fileType)
+          .then(() => {
+            lightGallery.current.closeGallery()
+          })
           .then(() => {
             router.refresh()
-            lightGallery.current.refresh()
           })
       }
     })
-  }
+  }, [list])
 
   return (
     <div>
       <LightGallery
         elementClassNames='gallery-methods-demo gallery-view grid grid-cols-gallery grid-rows-gallery [&>div]:bg-black [&>div]:rounded-xl gap-5 overflow-y-auto px-7 pr-4 mr-3 md:pl-14 md:mr-6 md:pr-8 md:grid-cols-gallery_md md:grid-rows-gallery_md'
         speed={500}
-        onAfterAppendSubHtml={onSlideItemLoad}
         onInit={onInit}
         plugins={[lgZoom, lgThumbnail, video, vimeoThumbnail]}
       >
