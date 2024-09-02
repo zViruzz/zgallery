e09af18597093163a4675f20c3742dd82e57a99e
Viruzz <nahuelsinka@gmail.com>
2024-09-02 04:36:57 -0300

    update

--- src/app/api/payment/route.ts
+++ src/app/api/payment/route.ts
@@ -1,19 +1,19 @@
-// import MercadoPagoConfig from 'mercadopago'
+import MercadoPagoConfig, { PreApprovalPlan } from 'mercadopago'
 import { type NextRequest } from 'next/server'
 
 export async function POST (request: NextRequest) {
   if (process.env.MP_ACCESS_TOKEN === undefined) {
     throw new Error('MP_ACCESS_TOKEN is not defined')
   }
-  // const client = new MercadoPagoConfig({
-  //   accessToken: process.env.MP_ACCESS_TOKEN
-  // })
+  const client = new MercadoPagoConfig({
+    accessToken: process.env.MP_ACCESS_TOKEN
+  })
 
-  const body = await request.json()
+  const id = await request.json()
+    .then((data) => data.data.id as string)
 
-  console.log('🚀 ~ POST ~ body:', body)
-  // const payment = await new PreApprovalPlan(client).get({ preApprovalPlanId: 'asdasdasd22' })
-  // console.log('🚀 ~ POST ~ payment:', payment)
+  const payment = await new PreApprovalPlan(client).get({ preApprovalPlanId: id })
+  console.log('🚀 ~ POST ~ payment:', payment)
 
   return Response.json({ success: true, status: 200 })
 }
