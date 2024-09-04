5eef4215295f55e6ff314de9a4a97a02d5be8688
Viruzz <nahuelsinka@gmail.com>
2024-09-04 13:32:22 -0300

    fixed

--- src/app/api/payment/route.ts
+++ src/app/api/payment/route.ts
@@ -1,24 +1,24 @@
-import MercadoPagoConfig, { PreApproval } from "mercadopago";
-import { type NextRequest } from "next/server";
+import MercadoPagoConfig, { PreApproval } from 'mercadopago'
+import { type NextRequest } from 'next/server'
 
-export async function POST(request: NextRequest) {
+export async function POST (request: NextRequest) {
   if (process.env.MP_ACCESS_TOKEN === undefined) {
-    throw new Error("MP_ACCESS_TOKEN is not defined");
+    throw new Error('MP_ACCESS_TOKEN is not defined')
   }
   const client = new MercadoPagoConfig({
-    accessToken: process.env.MP_ACCESS_TOKEN,
-  });
+    accessToken: process.env.MP_ACCESS_TOKEN
+  })
 
   const res = await request.json()
   const id = res.data.id as string
-  console.log("🚀 ~ POST ~ res:", res);
-  console.log("🚀 ~ POST ~ ID:", id);
+  console.log('🚀 ~ POST ~ res:', res)
+  console.log('🚀 ~ POST ~ ID:', id)
 
-  const payment = await new PreApproval(client).get({ id: id })
+  const payment = await new PreApproval(client).get({ id })
     .then(res => res)
-    .catch(res => console.error(res))
+    .catch(res => { console.error(res) })
 
-  console.log("🚀 ~ POST ~ resPayment:", payment);
+  console.log('🚀 ~ POST ~ resPayment:', payment)
 
-  return Response.json({ success: true, status: 200 });
+  return Response.json({ success: true, status: 200 })
 }
